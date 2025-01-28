86723e9499bf8b633c4d66ee723b67d23fa0df04
Shuhaib77 <shuhaibbasheer20022@gmail.com>
2025-01-28 23:49:05 +0530

    updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Header() {
+    const [menu,setMenu]=useState(false)
   return (
     <>
       <div className="">
@@ -13,12 +14,33 @@ function Header() {
             />
           </div>
 
-          <div className="flex w-full justify-around items-center h-[20px] mt-7">
-            <h1>HOME</h1>
-            <h1 className="text-blue-500">MENU</h1>
-            <h1>MAKE A RESERVATION</h1>
-            <h1>CONTACT US</h1>
+          <div className="flex w-full justify-around items-center h-[20px] mt-7  ">
+            <h1 className="md:block hidden">HOME</h1>
+            <h1 className="text-blue-500 md:block hidden">MENU</h1>
+            <h1 className="md:block hidden">MAKE A RESERVATION</h1>
+            <h1 className="md:block hidden">CONTACT US</h1>
           </div>
+          <div className="  lg:w-25%   ">
+
+        </div>
+        <div className="lg:hidden"> 
+        <i
+          class="fa-solid fa-bars fa-2xl "
+          style={{ color: "white" }}
+          onClick={() => {
+            menu ? setMenu(false) : setMenu(true);
+          }}
+        ></i>
+      </div>
+      {
+        menu &&
+        <div className="h-[100px] w-full backdrop-blur-2xl inset-0 fixed  bg-amber-50">
+            <div> </div>
+
+
+        </div>
+      }
+     
            {/* <img src="https://s3-alpha-sig.figma.com/img/0571/aff9/d875fd6fec8f3801ba095cc39be0e4b1?Expires=1739145600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=H4vwq1U7XnDOaJ3lr5FKCsvwOCt3piCa60rsSY6IBHlnP7J9TaclErCl2NHRu5yEOr4JJn-XQX99I8K~1l5PCeXxiPZ-ng5jRROHbl1Es47NdlT4NeFI1k2MQ0he9Ot4E98vaewmeG9ZhLYjY-GXyKJo5DVj7RKCuq2Y7DUY6Kv-TnsjJADJwbbIEA3expSyMCzUIiejm~pBQ9KJ8Cm1MOHk77ZDCU4s0xPhGi706zrZe~b3WpvtVS0UZbft21TKYLJY6Mti3qdvfoNIU7H4i9uoadsHRIMJ3jr9iMxLZNbndHr0TUaix9xmAOvRu-KK49wjgYBW1acw5KgNchkG4A__" alt="" /
         <img src="https://s3-alpha-sig.figma.com/img/aefd/7aa0/f81b6208cb3da0f5ecc0f0d109ca4bd0?Expires=1739145600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=QgugunDEPtViaSZudg2x1SMkE~4uq1Rj1Xr~Vuy86Gu0NDg55Qxyz35IDi9634t6yAZp~hN-hgRAV9nDjTnBdP3TG3JJkCSkz2Nat2qazfH3aMvwSH-P7nClyPTKC83rTuv3W4X1qq9glpGdyLTndwRlqwqJHxuMkdb7y1NTNRqnJMwAqNycp~uk6Kyi~ahmiNB58yn27HXrFCjRDEgJBSDo3NzOG5qP6jUugF93yyLzmn6YZgEdKaYVwtTplEA0zp8CDA141u3x1UFVu--dsoVyYaRShXSjKdhOvLGi5~xEhMfNdpt6~sONibM74D9kKCSAPvfMLiuuvZT3UsL5uw__" alt="" />
         <img src="https://s3-alpha-sig.figma.com/img/53f3/e533/f37f4a258b3eea846bf145fb95b71dfd?Expires=1739145600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=YkTFjMUuehDIRRcl-s5GpwpoXDswdhDOftf81BHGsT2BTTVnEj4qviQ61kH8nHrbEroruETjEhW3zGENTuEE9WcGzfSKLclF248jsfYSkANQOQ2NnpF1EbPCuaP54br68sMC6KbN16kmm4csiSGylYLhC~zRWGcSjrcdh~wYry6HVjWYctH7wJMsiji-GsfVCD551Ac34hrydfzWGqHqoxyF8hQwAmwV8wJbOpH803OfeIOqeYhUm7hIDXbYu8~lvnQMUlz97aMnqRo3QSjKURd81-~UWlSK~8oLTh1iiMg-cuNhMEZRl7MCucNhHkz4mba-sg5BTMjPooAn~aZ6ZQ__" alt="" />
--- src/components/menu/Viewmenu.jsx
+++ src/components/menu/Viewmenu.jsx
@@ -3,70 +3,55 @@ import React from "react";
 function Viewmenu() {
   return (
     <>
-      <div className="  lg:p-30">
-        <div className="lg:absolute left-12 bottom-0 sm:absolute ">
-          <img
-            className="w-[190px] h-[281px]"
-            src="https://s3-alpha-sig.figma.com/img/08f1/ba92/09e436820849a421ec0b1fe5126bf9b5?Expires=1739145600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=o5WMwELho~sx6kPQyhSX0W7JpIxIck3hhu2L7ar7Px0afPRnsclLbUM7JyGKw6P8XPWUB3EOEYalJ17N480QP5OmLeBHvf3x6EqzgnYlmm8DaKnpj51na2UbnIJDZEc5rb7njd5Kyeu1S6uHzZ9FW3cFjcoHr2lnUalPcDbqKq5OFm-8KBOMT8cVMcIRfCHNyCFbI3U9sRO5HqXftOF4AsGkUt6-x2~hSK4dCXzs0vkpagi9t3pw6wQQB~SU~xew7Rwp0sDBJz-Zimf822GoTyIl6R8k2JtIxUvEhlUkz9nf5EwbsasmP~8~C87CI1SFAH5ZqFdwo7g12WNvyF~rCQ__"
-            alt="Decorative"
-          />
-        </div>
-
-        <div className="flex flex-col  h-[500px] w-full p-10 border rounded-lg  overflow-auto">
-          <div className=" flex  items-center gap-y-10 justify-center  ">
-            <h1 className="text-5xl  mb-3 text-center font-extrabold">
-              BRUNCH COCKTAILS
-            </h1>
-          </div>
-          <div className="h-1 lgrid lg:grid-cols-2  sm: grid grid-cols-1 gap-3 gap-y-10 lg:m-10 w-full z-10  place-items-center  ">
-            <div className="flex flex-col justify-center ">
-              <h2 className="text-2xl font-bold text-white">
-                CINNAMON TOAST CRUNCH..........................$16
-              </h2>
-              <p class="break-words">
-                Skrewball peanut butter whiskey, vanilla extract, Amaretto,
-                Baileys, egg white, cinnamon
-              </p>
-            </div>
-            <div className="flex flex-col justify-center">
-              <h2 className="text-2xl font-bold text-white">
-                CINNAMON TOAST CRUNCH..........................$16
-              </h2>
-              <p class="break-words">
-                Skrewball peanut butter whiskey, vanilla extract, Amaretto,
-                Baileys, egg white, cinnamon
-              </p>
-            </div>
-            <div className="flex flex-col justify-center">
-              <h2 className="text-2xl font-bold text-white">
-                CINNAMON TOAST CRUNCH..........................$16
-              </h2>
-              <p class="break-words">
-                Skrewball peanut butter whiskey, vanilla extract, Amaretto,
-                Baileys, egg white, cinnamon
-              </p>
-            </div>
-            <div className="flex flex-col justify-center ">
-              <h2 className="text-2xl font-bold text-white">
-                CINNAMON TOAST CRUNCH..........................$16
-              </h2>
-              <p class="break-words">
-                Skrewball peanut butter whiskey, vanilla extract, Amaretto,
-                Baileys, egg white, cinnamon
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className="relative top-7  w-full">
-          <div className="absolute right-0 bottom-0">
-            <img
-              className="w-[190px] h-[281px]"
-              src="https://s3-alpha-sig.figma.com/img/6b91/b238/f07a69022d4c64e313237eadaceb997f?Expires=1739145600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=QIWCajpMaKaiolVzucZv5t13~zps0ftWEcbbOlPfvjgXGTKFnL3m-b1G~RefmmfWupO4iLowXH4CxX4cH5F4Vg50Am~fcEpW38dE6FMNK-IKa8GEs6~W4eQzPgy2hRuugNP8WjJtwhxBSal8D4JTPc9n24KeVhHB2mgN1bi6i4JxlLhqxHVGZqt0c4xMDVAh~1TJEXuqwdd8IRojDuOey96EayLsuD9gJATSnqOs~BWHufftlHQmBM2rLemzr6cQxNXEoxHab4-KD4X-mnYHYFSD~vjuDc6UgwsEY5MaBZ0OZekcjoKz0IFn3r5Pyriy106im~uZRXo-VcFBAxEFeQ__"
-              alt="Decorative"
-            />
-          </div>
-        </div>
+<div className="relative p-10 lg:p-30">
+  {/* Left Image */}
+  <div className="absolute left-4 top-4 lg:left-18 lg:top-0">
+    <img
+      className="w-[130px] h-[100px] lg:w-[190px] lg:h-[281px] object-contain"
+      src="https://s3-alpha-sig.figma.com/img/08f1/ba92/09e436820849a421ec0b1fe5126bf9b5?Expires=1739145600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=o5WMwELho~sx6kPQyhSX0W7JpIxIck3hhu2L7ar7Px0afPRnsclLbUM7JyGKw6P8XPWUB3EOEYalJ17N480QP5OmLeBHvf3x6EqzgnYlmm8DaKnpj51na2UbnIJDZEc5rb7njd5Kyeu1S6uHzZ9FW3cFjcoHr2lnUalPcDbqKq5OFm-8KBOMT8cVMcIRfCHNyCFbI3U9sRO5HqXftOF4AsGkUt6-x2~hSK4dCXzs0vkpagi9t3pw6wQQB~SU~xew7Rwp0sDBJz-Zimf822GoTyIl6R8k2JtIxUvEhlUkz9nf5EwbsasmP~8~C87CI1SFAH5ZqFdwo7g12WNvyF~rCQ_"
+      alt="Decorative Left"
+    />
+  </div>
+  {/* Main Content */}
+  <div className="flex flex-col h-full w-full p-5 lg:p-10 border rounded-lg overflow-auto">
+    <div className="flex items-center justify-center lg:mb-20">
+      <h1 className="text-3xl lg:text-5xl text-center font-extrabold">BRUNCH COCKTAILS</h1>
+    </div>
+    <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 lg:gap-10">
+      {/* Repeated Content */}
+      <div className="flex flex-col">
+        <h2 className="text-xl lg:text-2xl font-bold text-white">CINNAMON TOAST CRUNCH..........................$16</h2>
+        <p>Skrewball peanut butter whiskey, vanilla extract, Amaretto, Baileys, egg white, cinnamon</p>
       </div>
+      <div className="flex flex-col">
+        <h2 className="text-xl lg:text-2xl font-bold text-white">CINNAMON TOAST CRUNCH..........................$16</h2>
+        <p>Skrewball peanut butter whiskey, vanilla extract, Amaretto, Baileys, egg white, cinnamon</p>
+      </div>
+      <div className="flex flex-col">
+        <h2 className="text-xl lg:text-2xl font-bold text-white">CINNAMON TOAST CRUNCH..........................$16</h2>
+        <p>Skrewball peanut butter whiskey, vanilla extract, Amaretto, Baileys, egg white, cinnamon</p>
+      </div>
+      <div className="flex flex-col">
+        <h2 className="text-xl lg:text-2xl font-bold text-white">CINNAMON TOAST CRUNCH..........................$16</h2>
+        <p>Skrewball peanut butter whiskey, vanilla extract, Amaretto, Baileys, egg white, cinnamon</p>
+      </div>
+      <div className="flex flex-col">
+        <h2 className="text-xl lg:text-2xl font-bold text-white">CINNAMON TOAST CRUNCH..........................$16</h2>
+        <p>Skrewball peanut butter whiskey, vanilla extract, Amaretto, Baileys, egg white, cinnamon</p>
+      </div>
+      {/* Add more items here */}
+    </div>
+  </div>
+  {/* Right Image */}
+  <div className="absolute right-4 bottom-4 lg:right-30 lg:bottom-18">
+    <img
+      className="w-[150px] h-[200px] lg:w-[190px] lg:h-[281px] object-contain"
+      src="https://s3-alpha-sig.figma.com/img/6b91/b238/f07a69022d4c64e313237eadaceb997f?Expires=1739145600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=QIWCajpMaKaiolVzucZv5t13~zps0ftWEcbbOlPfvjgXGTKFnL3m-b1G~RefmmfWupO4iLowXH4CxX4cH5F4Vg50Am~fcEpW38dE6FMNK-IKa8GEs6~W4eQzPgy2hRuugNP8WjJtwhxBSal8D4JTPc9n24KeVhHB2mgN1bi6i4JxlLhqxHVGZqt0c4xMDVAh~1TJEXuqwdd8IRojDuOey96EayLsuD9gJATSnqOs~BWHufftlHQmBM2rLemzr6cQxNXEoxHab4-KD4X-mnYHYFSD~vjuDc6UgwsEY5MaBZ0OZekcjoKz0IFn3r5Pyriy106im~uZRXo-VcFBAxEFeQ_"
+      alt="Decorative Right"
+    />
+  </div>
+</div>
+
     </>
   );
 }
